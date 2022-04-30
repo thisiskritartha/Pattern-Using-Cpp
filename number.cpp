e69872f8edb4155060c08c4f2d5c69018a219a91
Kritartha Bikram Shah <67311836+thisiskritartha@users.noreply.github.com>
2022-04-30 11:40:17 +0545

    Number Pattern2
        1
       1 2
      1 2 3
     1 2 3 4
    1 2 3 4 5

--- number.cpp
+++ number.cpp
@@ -7,12 +7,12 @@ int main(){
     cout<<"Enter the number of Rows."<<endl;
     cin>>n;
     for(int i=1; i<=n; i++){
-        for(int j=1; j<=(5-i); j++){
+        for(int j=1; j<=(n-i); j++){
             cout<<" ";
         }
         int sum = 1;
         for(int j=1; j<=i; j++){
-            cout<<sum;
+            cout<<sum<<" ";
             sum++;
         }
         cout<<endl;
