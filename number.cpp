d472ee403b513c67f05e7f0552e3f579535cbd10
Kritartha Bikram Shah <67311836+thisiskritartha@users.noreply.github.com>
2022-04-30 11:27:31 +0545

    Number Pattern.
        1
       12
      123
     1234
    12345

---
+++ number.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number of Rows."<<endl;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=(5-i); j++){
+            cout<<" ";
+        }
+        int sum = 1;
+        for(int j=1; j<=i; j++){
+            cout<<sum;
+            sum++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
